e3c064aaa835b55f67c413cb4a323ef023d5f527
rrbolton423 <bolton.romell423@gmail.com>
2020-02-17 18:58:12 -0600

    Add Track model and utility for JSON parsing

---
+++ app/src/main/java/com/romellbolton/putmeon/Track.java
@@ -0,0 +1,105 @@
+package com.romellbolton.putmeon;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Track implements Parcelable {
+    String name, artist, url, smallImageURL, largeImageURL, mbid;
+
+    public Track() { }
+
+    public Track(Parcel in) {
+        this.name = in.readString();
+        this.artist = in.readString();
+        this.url = in.readString();
+        this.smallImageURL = in.readString();
+        this.largeImageURL = in.readString();
+        this.mbid = in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSmallImageURL() {
+        return smallImageURL;
+    }
+
+    public void setSmallImageURL(String smallImageURL) {
+        this.smallImageURL = smallImageURL;
+    }
+
+    public String getLargeImageURL() {
+        return largeImageURL;
+    }
+
+    public void setLargeImageURL(String largeImageURL) {
+        this.largeImageURL = largeImageURL;
+    }
+
+    public String getMbid() {
+        return mbid;
+    }
+
+    public void setMbid(String mbid) {
+        this.mbid = mbid;
+    }
+
+    @Override
+    public String toString() {
+        String stTrack = "{" +
+                "\"name\":\"" + this.name + "\"," +
+                "\"artist\":\"" + this.artist + "\"," +
+                "\"url\":\"" + this.url + "\"," +
+                "\"smallImageURL\":\"" + this.smallImageURL + "\"," +
+                "\"largeImageURL\":\"" + this.largeImageURL + "\"," +
+                "\"mbid\":\"" + this.mbid + "\"}";
+
+        return stTrack;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(artist);
+        parcel.writeString(url);
+        parcel.writeString(smallImageURL);
+        parcel.writeString(largeImageURL);
+        parcel.writeString(mbid);
+
+    }
+    public static final Parcelable.Creator<Track> CREATOR = new Parcelable.Creator<Track>() {
+        public Track createFromParcel(Parcel in) {
+            return new Track(in);
+        }
+
+        public Track[] newArray(int size) {
+            return new Track[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/romellbolton/putmeon/TrackUtil.java
@@ -0,0 +1,127 @@
+package com.romellbolton.putmeon;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class TrackUtil {
+    static public class TrackJSONParser {
+        static ArrayList<Track> parseTracks(String in) throws JSONException {
+
+            JSONObject root = new JSONObject(in);
+            JSONArray JSONArrayTrack = null;
+            if (root.has("results")) {
+                JSONArrayTrack = root.getJSONObject("results").getJSONObject("trackmatches").getJSONArray("track");
+                return getSearchedTracks(JSONArrayTrack);
+            } else if (root.has("similartracks")) {
+                JSONArrayTrack = root.getJSONObject("similartracks").getJSONArray("track");
+                return getSimilarTracks(JSONArrayTrack);
+            }
+            return null;
+        }
+
+        static ArrayList<Track> getSearchedTracks(JSONArray JSONArrayTrack) throws JSONException {
+            ArrayList<Track> trackArrayList = new ArrayList<Track>();
+            for (int i = 0; i < JSONArrayTrack.length(); i++) {
+                JSONObject JSONObjectTrack = JSONArrayTrack.getJSONObject(i);
+                Track track = new Track();
+                track.setName(JSONObjectTrack.getString("name"));
+                track.setArtist(JSONObjectTrack.getString("artist"));
+                track.setUrl(JSONObjectTrack.getString("url"));
+                track.setSmallImageURL(JSONObjectTrack.getJSONArray("image").getJSONObject(0).getString("#text"));
+                track.setLargeImageURL(JSONObjectTrack.getJSONArray("image").getJSONObject(2).getString("#text"));
+                trackArrayList.add(track);
+            }
+            return trackArrayList;
+        }
+        static ArrayList<Track> getSimilarTracks(JSONArray JSONArrayTrack) throws JSONException {
+            ArrayList<Track> trackArrayList = new ArrayList<Track>();
+            for (int i = 0; i < JSONArrayTrack.length(); i++) {
+                JSONObject JSONObjectTrack = JSONArrayTrack.getJSONObject(i);
+                Track track = new Track();
+                track.setName(JSONObjectTrack.getString("name"));
+                track.setArtist(JSONObjectTrack.getJSONObject("artist").getString("name"));
+                track.setUrl(JSONObjectTrack.getString("url"));
+                track.setSmallImageURL(JSONObjectTrack.getJSONArray("image").getJSONObject(0).getString("#text"));
+                track.setLargeImageURL(JSONObjectTrack.getJSONArray("image").getJSONObject(2).getString("#text"));
+                trackArrayList.add(track);
+            }
+            return trackArrayList;
+        }
+    }
+
+    static ArrayList<Track> getFavouriteTracks(JSONArray JSONArrayTracks) throws JSONException {
+        ArrayList<Track> trackArrayList = new ArrayList<Track>();
+        for (int i = 0; i < JSONArrayTracks.length(); i++) {
+            JSONObject JSONObjectTrack = JSONArrayTracks.getJSONObject(i);
+            Track track = new Track();
+            track.setName(JSONObjectTrack.getString("name"));
+            track.setArtist(JSONObjectTrack.getString("artist"));
+            track.setUrl(JSONObjectTrack.getString("url"));
+            track.setSmallImageURL(JSONObjectTrack.getString("smallImageURL"));
+            track.setLargeImageURL(JSONObjectTrack.getString("largeImageURL"));
+            trackArrayList.add(track);
+        }
+        return trackArrayList;
+
+    }
+
+    static ArrayList<Track> getTracksFromPref(SharedPreferences pref) throws JSONException {
+        JSONArray jsonArray = new JSONArray(pref.getString("favourites", "[]"));
+        ArrayList<Track> trackArrayList = new ArrayList<Track>();
+        trackArrayList.addAll(getFavouriteTracks(jsonArray));
+        return trackArrayList;
+    }
+
+    static void addTrackToPreferences(Track track, Context mContext) {
+
+        String name = mContext.getPackageName();
+        SharedPreferences pref = mContext.getApplicationContext().getSharedPreferences(name, MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        if (pref.getString("favourites", null) == null) {
+            editor.putString("favourites", "[]");
+            editor.commit();
+        }
+
+        String jObj = pref.getString("favourites", null);
+        int index = jObj.lastIndexOf(']');
+        String jsonObject = null;
+
+        if (jObj.lastIndexOf('}') == jObj.length() - 2) {
+            jsonObject = jObj.substring(0, index).concat(", " + track.toString()) + "]";
+        } else {
+            jsonObject = jObj.substring(0, index).concat(track.toString()) + "]";
+        }
+        editor.putString("favourites", jsonObject);
+        editor.commit();
+    }
+
+    static ArrayList<Track> removeTrackFromPreferences(Track track, Context mContext) throws JSONException {
+        String name = mContext.getPackageName();
+        SharedPreferences pref = mContext.getApplicationContext().getSharedPreferences(name, MODE_PRIVATE);
+
+        ArrayList<Track> trackArrayList = new ArrayList<>();
+        trackArrayList.addAll(getTracksFromPref(pref));
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+        for (int i = 0; i < trackArrayList.size(); i++) {
+            Track t = trackArrayList.get(i);
+            if (t.getName().equalsIgnoreCase(track.getName())) {
+                trackArrayList.remove(t);
+            }
+        }
+        for (int i = 0; i < trackArrayList.size(); i++) {
+            Track t = trackArrayList.get(i);
+            addTrackToPreferences(t, mContext);
+        }
+        return trackArrayList;
+    }
+}
