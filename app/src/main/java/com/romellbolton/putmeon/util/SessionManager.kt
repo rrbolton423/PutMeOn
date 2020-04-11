0ec7027d2b0e6191908218255f3ad7956cdc854d
rrbolton423 <bolton.romell423@gmail.com>
2020-04-11 07:28:45 -0500

    Convert files from Java to Kotlin pt.2

--- app/src/main/java/com/romellbolton/putmeon/util/SessionManager.kt
+++ app/src/main/java/com/romellbolton/putmeon/util/SessionManager.kt
@@ -1,10 +1,7 @@
 package com.romellbolton.putmeon.util
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
-import com.romellbolton.putmeon.controller.LoginActivity
-import java.util.*
 
 class SessionManager {
     var PRIVATE_MODE = 0
@@ -15,34 +12,6 @@ class SessionManager {
         editor.putBoolean(IS_LOGIN, true)
         editor.putString("id", apiUserName)
         editor.commit()
-    }
-
-    val userDetails: HashMap<String, String>
-        get() {
-            val user = HashMap<String, String>()
-            user["id"] = pref.getString("id", null)
-            return user
-        }
-
-    fun checkLogin() {
-        if (!isLoggedIn) {
-            val i = Intent(context, LoginActivity::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context!!.startActivity(i)
-        }
-    }
-
-    val isLoggedIn: Boolean
-        get() = pref.getBoolean(IS_LOGIN, false)
-
-    fun logoutUser() {
-        editor.clear()
-        editor.commit()
-        val i = Intent(context, LoginActivity::class.java)
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context!!.startActivity(i)
     }
 
     companion object {
